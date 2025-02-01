9d9b5f6508929941e9d023baab3f8e4ee560deb6
Michael Pfister <michael.pfister@id.unibe.ch>
2025-02-01 23:11:07 +0000

    Refactor Kirby configuration: move license handling to site config and remove old config file

--- site/config/config.php
+++ site/config/config.php
@@ -1,4 +1,5 @@
 <?php
+use Kirby\Filesystem\F;
 
 /**
  * The config file is optional. It accepts a return array with config options
@@ -7,7 +8,17 @@
  * This setting must be set to false in production.
  * All config options: https://getkirby.com/docs/reference/system/options
  */
+
 return [
     'debug' => true,
     'yaml.handler' => 'symfony', // already makes use of the more modern Symfony YAML parser: https://getkirby.com/docs/reference/system/options/yaml (will become the default in a future Kirby version)
+    'url' => getenv('KIRBY_URL'),
+    'ready' => function($kirbyLicense) {
+        $license_file = $kirbyLicense->root('license');
+        $license = getenv('KIRBY_LICENSE');
+
+        if ($license && !F::exists($license_file)) {
+            F::write($license_file, $license);
+        }
+    }
 ];
