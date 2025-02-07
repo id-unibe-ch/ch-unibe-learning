ad6fcbfa4f4de3196390d71d49145e23852c63c7
Michael Pfister <michael.pfister@id.unibe.ch>
2025-02-07 00:04:00 +0000

    Add OAuth configuration for University of Bern with user access controls

--- site/config/config.php
+++ site/config/config.php
@@ -28,6 +28,47 @@ return [
     'panel' =>[
         'install' => true
     ],
+    'thathoff' => [
+        'oauth' => [
+            'providers' => [
+                'custom' => [
+                    // this one uses \League\OAuth2\Client\Provider\GenericProvider automatically
+                    'name'                    => 'University of Bern', // The name is optional
+                    'clientId'                => getenv('KIRBY_OIDC_CLIENT_ID'),    // The client ID assigned to you by the provider
+                    'clientSecret'            => getenv('KIRBY_OIDC_CLIENT_SECRET'),   // The client password assigned to you by the provider
+                    'redirectUri'             => getenv('KIRBY_OIDC_REDIRECT_URI'),
+                    'urlAuthorize'            => getenv('KIRBY_OIDC_URL_AUTHORIZE'),
+                    'urlAccessToken'          => getenv('KIRBY_OIDC_URL_ACCESS_TOKEN'),
+                    'urlResourceOwnerDetails' => getenv('KIRBY_OIDC_URL_RESOURCE_OWNER_DETAILS'),
+                    'icon'                    => 'account',  // Pick any default Kirby icon for the login button (optional)
+                    'scope'                   => 'openid email profile User.Read'  //specify the scope passed form the OIDC provider to kirby
+                ],
+            ],
+        ],
+        // Only allow logins for existing kirby users (don’t create new users)
+        'onlyExistingUsers' => false,
+
+         // Set the default role of newly created users.
+        'defaultRole' => 'nobody',
+
+        // Allow every valid user of all OAuth providers to login.
+        // For details see “Configure Allowed Users” below.
+        // DANGEROUS: Make sure you know what you’re doing when setting this to true!
+        'allowEveryone' => false,
+
+        // List of E-mail domains which are allowed to login
+        'domainWhitelist' => [
+          'unibe.ch'
+        ],
+
+        // List of E-mail addresses which are allowed to login
+        'emailWhitelist' => [
+          // For details see “Configure Allowed Users” below.
+        ],
+
+        // Remove the standard Kirby login form and only display OAuth options.
+        'onlyOauth' => true,
+    ],
     'ready' => function($kirbyLicense) {
         $license_file = $kirbyLicense->root('license');
         $license = getenv('KIRBY_LICENSE');
