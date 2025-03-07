f700a0c58108bc23581ea9f7a01e9f6d735b6913
Michael Pfister <michael.pfister@unibe.ch>
2025-03-07 12:47:56 +0100

    Add new documentation and tutorials blueprints; configure markdown content extension

---
+++ site/templates/docs.php
@@ -0,0 +1,56 @@
+<?php
+
+  $backgroundHeader = $site->headerBackground()->toStructure()->first();
+  $backgroundPage = $page->background()->toStructure()->first();
+
+?>
+<?php snippet('header') ?>
+
+  <?php
+
+    // Page background class
+
+    if ($backgroundPage) {
+      $backgroundPageClass = background($backgroundPage);
+    }
+
+    // Classes
+
+    if ($backgroundPage && $backgroundPageClass) {
+      $class = $backgroundPageClass . ' page-' . $page->slug();
+    } else {
+      $class = 'page-' . $page->slug();
+    }
+
+    if ($backgroundHeader && background($backgroundHeader)) {
+      $class = $class . ' header-bg';
+
+      if ($site->headerPosition()->value() === 'default') {
+        $class = $class . ' header-default';
+      } elseif ($site->headerPosition()->value() === 'fixed') {
+        $class = $class . ' header-fixed';
+      } elseif ($site->headerPosition()->value() === 'fixed--sm') {
+        $class = $class . ' header-fixed header-fixed--sm';
+      }
+    } elseif ($site->headerPosition()->value() === 'default') {
+      $class = $class . ' header-default';
+    } elseif (in_array($site->headerPosition()->value(), ['fixed', 'fixed--sm'])) {
+      $class = $class . ' header-fixed header-fixed--sm';
+    }
+
+    if ($backgroundPage && $backgroundPage->brightness()->bool()) {
+      $class = $class . ' dark';
+    }
+
+  ?>
+  <main class="<?= $class ?>">
+
+    <div class="hero-none">
+
+      <?php snippet('blocks', ['page' => $page->text()->markdown()]) ?>
+
+    </div>
+
+  </main>
+
+<?php snippet('footer') ?>
