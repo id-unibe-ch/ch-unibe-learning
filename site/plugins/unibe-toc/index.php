126b40b9e2765d1f55de3f959e270e644e551166
Michael Pfister <michael.pfister@unibe.ch>
2025-03-09 13:55:25 +0100

    Add table of contents plugin and update configuration for headlines

---
+++ site/plugins/unibe-toc/index.php
@@ -0,0 +1,32 @@
+<?php
+
+Kirby::plugin('unibe/toc', [
+  'hooks' => [
+    'kirbytext:after' => [
+
+      function($text) {
+
+        // get the headline levels to convert from a config option, we use h2 as the default
+        $headlines = option('unibe.toc.headlines', 'h2|h3');
+
+        // create the regex pattern to be used as first argument in `preg_replace_callback()`
+        $headlinesPattern = is_array($headlines) ? implode('|', $headlines) : $headlines;
+
+        // use `preg_replace_callback()` to replace matches with anchors
+        $text = preg_replace_callback('!<(' . $headlinesPattern . ')>(.*?)</\\1>!s', function ($match) {
+
+            // create the id from the headline text
+            $id = Str::slug(Str::unhtml($match[2]));
+
+            // return the modified headline:
+            // $match[1] contains the match for the first subpattern, i.e. `h2`, `h3` etc.
+            // $match[2] contains the match for the second subpattern, i.e. the actual headline text
+            return '<' . $match[1] . ' id="' . $id . '"><a href="#' . $id . '">' . $match[2] . '</a></' . $match[1] . '>';
+
+        }, $text);
+
+        return $text;
+      },
+    ]
+  ]
+]);
